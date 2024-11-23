0018030bb1dd439b27832a21914a8ee8c0057d46
ibrahim musa <miharbiibra675@gmail.com>
2024-11-23 22:20:37 +0300

    studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router()
 const studentController = require("../controllers/studentController");
+const { verifyAccessToken, restrict } = require("../helpers/jwtHelper");
 
     
 router.post('/addStudent', studentController.addStudent);
 
-router.get('/getAllStudents', studentController.getAllStudents);
-router.get('/getStudent/:student_id', studentController.getStudent);
-router.get('/getAllStudentwithCourse', studentController.getAllStudentwithCourse);
-router.get('/getoneStudentwithCourse/:student_id', studentController.getOneStudentWithCourse);
+router.get('/getAllStudents', verifyAccessToken, restrict("admin","user"), studentController.getAllStudents);
+router.get('/getStudent/:student_id', verifyAccessToken, restrict("admin","user"), studentController.getStudent);
+// router.get('/getAllStudentwithCourse', studentController.getAllStudentwithCourse);
+// router.get('/getoneStudentwithCourse/:student_id', studentController.getOneStudentWithCourse);
 
-router.patch('/updateStudent/:student_id', studentController.updateStudent);
+router.patch('/updateStudent/:student_id', verifyAccessToken, restrict("admin"),studentController.updateStudent);
 
-router.delete('/deleteStudent/:student_id', studentController.deleteStudent);
+router.delete('/deleteStudent/:student_id', verifyAccessToken, restrict("admin"), studentController.deleteStudent);
 
 
 module.exports = router
